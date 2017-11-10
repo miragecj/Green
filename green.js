a8eb6417d287dae31f7e1443fc2216792eb0606c
Stefana Boldan (Codau) <stefana@umtsoftware.com>
2017-11-10 16:20:58 +0200

    sdsd

--- green.js
+++ green.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
-    var trash = $("#garbage");
-    var trashPosition  = $("#garbage").position().left - $("#man").width();
-     TweenMax.to("#man", 10, {left:trashPosition});
-     TweenMax.to("#trash", 3, {x:80, y:80, delay:10, opacity:0});
+    ThrowAway($("#trash"), $("#garbage"));
+    
 });
+
+
+var ComputeThrowPosition = function(garbageBin){
+    return garbageBin.position().left - $("#man").width();
+}
+
+var ThrowAway = function(pieceOfTrash, garbageBin){
+    //move man with trash next to the bin
+    TweenMax.to("#man", 10, {left:ComputeThrowPosition(garbageBin)});
+    //throw trash into bin
+    TweenMax.to(pieceOfTrash, 3, {x:80, y:80, delay:10, opacity:0});
+}
