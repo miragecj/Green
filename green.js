404648de42263273b0b6365e04414aac1958591c
Daniela Galatanu <daniela@umtsoftware.com>
2017-11-10 09:11:46 -0800

    cheee

--- green.js
+++ green.js
@@ -1,44 +1,45 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $(".bin").click(OnBinClick);
- var mappings = 
-     [
-         {
-            "ObjectId": "glassTrash",
-            "ContainerId":"#garbage2"
-         },
-        {
-            "ObjectId": "plasticTrash",
-            "ContainerId":"#garbage3"
-         },
-        {
-            "ObjectId": "paperTrash",
-            "ContainerId":"#garbage1"
-        }
-     ];
+    var mappings =
+        [
+            {
+                "ObjectId": "glassTrash",
+                "ContainerId": "#garbage2"
+            },
+            {
+                "ObjectId": "plasticTrash",
+                "ContainerId": "#garbage3"
+            },
+            {
+                "ObjectId": "paperTrash",
+                "ContainerId": "#garbage1"
+            }
+        ];
     var selectedMapping = mappings[0];
-     $("#trash").addClass(selectedMapping.ObjectId)
-  
-    MoveCloud();
+    $("#trash").addClass(selectedMapping.ObjectId)
+
+    MoveClouds();
     ThrowAway($("#trash"), $(selectedMapping.ContainerId));
-    
+
 });
 
 
-var ComputeThrowPosition = function(garbageBin){
+var ComputeThrowPosition = function (garbageBin) {
     return garbageBin.position().left - $("#man").width();
 }
 
-var ThrowAway = function(pieceOfTrash, garbageBin){
+var ThrowAway = function (pieceOfTrash, garbageBin) {
     //move man with trash next to the bin
-    TweenMax.to("#man", 10, {left:ComputeThrowPosition(garbageBin)});
+    TweenMax.to("#man", 10, { left: ComputeThrowPosition(garbageBin) });
     //throw trash into bin
-    TweenMax.to(pieceOfTrash, 3, {x:80, y:80, delay:10, opacity:0});
+    TweenMax.to(pieceOfTrash, 3, { x: 80, y: 80, delay: 10, opacity: 0 });
 }
 
-function MoveCloud(){
-    TweenMax.to(".norisor", 50, { left: "700px", repeat: 10, yoyo: true, onRepeat: function() { }, repeatDelay: 0.5, ease: Linear.easeNone });
+function MoveClouds() {
+        var distance = $("#garbage2").position().left;
+        TweenMax.to(".cloud", 30, { left: distance, repeat: 10, yoyo: true, onRepeat: function () { }, repeatDelay: 0.5, ease: Linear.easeNone });
 }
 
-var OnBinClick = function(){
-alert(this.id);
+var OnBinClick = function () {
+    alert(this.id);
 }
